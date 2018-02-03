92e08a925d44f2ec05fa9a43a76b786b67126c08
Guttesen <og1806x9@hotmail.com>
2018-02-03 17:32:37 +0100

    added a few changes

--- simple_db.rb
+++ simple_db.rb
@@ -11,6 +11,8 @@ puts "Here is the list of commands."
 puts 'set: saves data to the memory. Example: set 1 "test data".'
 puts "commit: saves data to the database."
 puts "get: loads data from database. Example: get 1."
+puts "getall: prints all the elements in the database."
+puts "clear: Clears the database and the hash."
 puts "exit: exits the program. Example: exit."
 puts "help: prints the commands again."
 while(is_running) do
@@ -28,7 +30,6 @@ while(is_running) do
 		unless command[1].to_i.to_s == command[1] then
 	  	puts "The key should be a Integer."
 	  else
-	  	puts "You didn't specify a number."
 			unless data then
 				puts "No data to set, please specify data. Maybe you forgot to add quotation marks."
 			else
@@ -36,20 +37,35 @@ while(is_running) do
 		  	puts "The value " + data + " was added to the database."
 			end
 		end
+	when "getall"
+		puts "Here is a list of all the key value pairs in the hash."
+		puts "-------------------------------------------------------"
+		db_hash.each do |key, value|
+  		puts "#{key}: #{value}"
+		end
+		puts "-------------------------------------------------------"
 	when "commit"
 		db_hash.each do |k, v|
 			gdbm[k] = v
 		end
 		puts "Data in hash is saved to the database."
+	when "clear"
+		gdbm.clear()
+		db_hash = {}
+		puts "The database and the hash is cleared."
 	when "exit"
 		is_running = false
 	when "help"
+		puts "-----------------------------------------------------------"
 	  puts "Here is the list of commands."
 		puts 'set: saves data to the memory. Example: set 1 "test data".'
 		puts "commit: saves data to the database."
 		puts "get: loads data from database. Example: get 1."
+		puts "getall: prints all the elements in the database."
+		puts "clear: Clears the database and the hash."
 		puts "exit: exits the program. Example: exit."
 		puts "help: prints the commands again."
+		puts "-----------------------------------------------------------"
 	else
 	  puts "You have given a wrong command."
 	end
